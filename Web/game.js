edab284611b9a6e9fdbbfa325d3bfdff1afec7a1
Matheus Siqueira Fernandes <matheussique@outlook.com>
2022-10-12 23:04:49 -0300

    Atualização do jogo para o site

--- Web/game.js
+++ Web/game.js
@@ -1,17 +1,32 @@
 var temp = null;
+var ctempo = null;
+var tamanho = null;
+var trapaca = 0;
+var jaEscolheu = 0;
+var modo = null;
+var acertos = 0;
 
 function start(){
 	let query = window.location.search;
 	let info = new URLSearchParams(query);
-	let tamanho = info.get('tamanho');
-	
-	var cartas = makeCards(tamanho);
+	tamanho = info.get('tamanho');
+	modo = info.get('modo');
+	var cartas = makeCards();
 		
 	shuffleArray(cartas);
-	
-	//console.log(cartas);	
-	
-	criaTabuleiro(tamanho, cartas);
+	criaTabuleiro(cartas);
+  if(modo == 'temp'){
+    inicializaTimer();
+  }
+  /*-------------------------------------------------------------
+    Adiciona Triggers
+  -------------------------------------------------------------*/
+
+  let dis = document.getElementById("desistir");
+  dis.addEventListener("click",desistir,"false");
+
+  let trap = document.getElementById("trapaca");
+  trap.addEventListener("click",ativaTrapaca,"false");
 }
 
 function shuffleArray(array) {
@@ -21,8 +36,9 @@ function shuffleArray(array) {
     }
 }
 
-function makeCards(tamanho){
+function makeCards(){
 	let numCarta = (tamanho/2)*tamanho;
+  childs = numCarta;
 	let cartas=[];
 	
 	
@@ -33,7 +49,7 @@ function makeCards(tamanho){
 	return cartas;
 }
 
-function criaTabuleiro(tamanho, cartas){
+function criaTabuleiro(cartas){
 	let tbl = document.getElementById("jogo");
 	let cons = 0;
 	let num = 0;
@@ -88,29 +104,32 @@ function criaTabuleiro(tamanho, cartas){
 
 function verificaParidade(event){
 	let carta = document.getElementById(event.target.id);
+  let ponto = document.getElementById("pontos")
+  let Patual = parseInt(ponto.innerHTML,10)
+
+  if(!jaEscolheu && modo == 'temp'){
+    criaTimer(tamanho);
+    jaEscolheu = 1;
+  }
 
 	if((carta.name != null) && (carta.name != "costa")){
 		flipCarta(event.target.id)
 		if(temp == null){
-			temp = [carta.name,carta.id];
-			console.log(temp)
+			temp = [carta.name,carta.id]
+      console.log(temp)
 		}else{
 			if(temp[0] == carta.name){
-				window.alert("Acertou")
 				temp = null;
+        ponto.innerHTML=Patual+1;
+        setTimeout(verificaVitoria,701);
 			}else{
 				let par = document.getElementById(temp[1])
-				
-				console.log(temp)
 				deFlipCarta(event.target.id);
-				deFlipCarta(temp[1]);	
-				
+				deFlipCarta(temp[1]);
+        ponto.innerHTML=Patual+1;
 				temp = null;
-
-				window.alert("Errou")
 			}
-		}
-		
+		}		
 	}
 }
 
@@ -134,20 +153,108 @@ function deFlipCarta(id){
 		
 }
 
+function criaTimer(){
+  switch(tamanho){
+    case '2':
+      ctempo = setInterval(reduzTimer,1000);
+      setTimeout(finalizaJogo,20*1000);
+      break;
+
+    case '4':
+      ctempo = setInterval(reduzTimer,1000);
+      setTimeout(finalizaJogo,60*1000);
+      break;
+
+    case '6':
+      ctempo = setInterval(reduzTimer,1000);
+      setTimeout(finalizaJogo,90*1000);
+      break;
+
+    case '8':
+      ctempo = setInterval(reduzTimer,1000);
+      setTimeout(finalizaJogo,120*1000);
+     
+      break;
+  }
+}
+
+function finalizaJogo(){
+  clearInterval(ctempo);
+  if(confirm("Você perdeu.\nDeseja jogar novamente?")){
+    document.location.reload()
+  }else{
+    window.open("../Ejogo/Ejogo.html","_self");
+  }
+}
+
+function reduzTimer(){
+  let t = document.getElementById("Timer");
+  let tAtual = parseInt(t.innerHTML,10);
+  t.innerHTML=tAtual-1;
+}
+
+function desistir(){
+  if(confirm("Você realmente dejesa sair?")){
+    window.open("../Ejogo/Ejogo.html","_self");
+  }
+}
+
+function ativaTrapaca(){
+  console.log(childs)
+
+  if(!trapaca){
+    trapaca = 1;
+
+    for(let i = 0;i<(childs*childs);i++){
+      let carta = document.getElementById("C"+i).parentNode.parentNode;
+      if(!carta.classList.contains("carta_rodada")){
+        carta.classList.add("carta_trap");
+      }
+    }
+  }else{
+    trapaca = 0;
+
+    for(let i = 0;i<(childs*childs);i++){
+      let carta = document.getElementById("C"+i).parentNode.parentNode;
+      if(!carta.classList.contains("carta_rodada")){
+        carta.classList.remove("carta_trap");
+      }
+    }
+  }
+}
+
+function inicializaTimer(){
+  let timer = document.getElementById("Timer");
+
+  timer.parentNode.classList.remove("disable");
+  switch(tamanho){
+    case '2':
+      timer.innerHTML=20;
+      break;
+
+    case '4':
+      timer.innerHTML=60;
+      break;
+
+    case '6':
+      timer.innerHTML=90;
+      break;
+
+    case '8':
+      timer.innerHTML=120;
+      break;
+  }
+}
+
+function verificaVitoria(){
+  acertos++;
+  if(acertos==childs){
+    if(confirm("Você Venceu ^-^,\n Deseja jogar novamente?")){
+      document.location.reload();
+    }else{
+      window.open("../Ejogo/Ejogo.html","_self");
+    }
+  }
+}
+
 start();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
